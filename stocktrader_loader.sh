ecda215631d7d0450c5e7c56c05b51cd1707e4ac
Brian Stump <Brian.Stump@ibm.com>
2017-10-18 13:47:29 -0400

    Fixed setting environment variables.

--- stocktrader_loader.sh
+++ stocktrader_loader.sh
@@ -26,9 +26,9 @@ then
 fi
 
 STOCKTRADER_LOCATION=$(dirname "$0")
-DS_USERNAME=admin
-DS_PASSWORD=admin
-DS_WEB_URL=https://localhost:8443
+export DS_USERNAME=admin
+export DS_PASSWORD=admin
+export DS_WEB_URL=https://localhost:8443
 
 echo loading plugins
 echo
@@ -55,28 +55,28 @@ echo
 echo importing templates
 echo
 
-curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/tmp/Docker+Template.json;type=application/zip" -F "filename=Docker+Template.json" $DS_WEB_URL/rest/deploy/componentTemplate/import 1> /dev/null
-curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/tmp/KubernetesApplicationTemplate.json;type=application/zip" -F "filename=KubernetesApplicationTemplate.json" $DS_WEB_URL/rest/deploy/applicationTemplate/import?resourceTemplateUpgradeType=UPGRADE_IF_EXISTS 1> /dev/null
+curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/tmp/Docker+Template.json;type=application/zip" -F "filename=Docker+Template.json" $DS_WEB_URL/rest/deploy/componentTemplate/import > /dev/null
+curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/tmp/KubernetesApplicationTemplate.json;type=application/zip" -F "filename=KubernetesApplicationTemplate.json" $DS_WEB_URL/rest/deploy/applicationTemplate/import?resourceTemplateUpgradeType=UPGRADE_IF_EXISTS > /dev/null
 
 echo creating team
 echo
 
-$udclient createTeam -team "Container Team" 1> /dev/null
+$udclient createTeam -team "Container Team" > /dev/null
 
 echo creating components
 echo 
 
-curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/Component/stocktrader-all-in-one.yaml.json;type=application/zip" -F "filename=stocktrader-all-in-one.yaml.json" $DS_WEB_URL/rest/deploy/component/import 1> /dev/null
+curl -k —verbose -u $DS_USERNAME:$DS_PASSWORD -s --insecure -F "file=@$STOCKTRADER_LOCATION/Component/stocktrader-all-in-one.yaml.json;type=application/zip" -F "filename=stocktrader-all-in-one.yaml.json" $DS_WEB_URL/rest/deploy/component/import > /dev/null
 
 echo creating component versions
 echo
-$udclient createVersion -component stocktrader-all-in-one.yaml -name v1 1> /dev/null
-$udclient createVersion -component stocktrader-all-in-one.yaml -name v1-onprem-db 1> /dev/null
+$udclient createVersion -component stocktrader-all-in-one.yaml -name v1 > /dev/null
+$udclient createVersion -component stocktrader-all-in-one.yaml -name v1-onprem-db > /dev/null
 
 echo adding files to versions
 echo
-$udclient addVersionFiles -component stocktrader-all-in-one.yaml -version v1 -base $STOCKTRADER_LOCATION/Component/v1/ 1> /dev/null
-$udclient addVersionFiles -component stocktrader-all-in-one.yaml -version v1-onprem-db -base $STOCKTRADER_LOCATION/Component/v1-onprem-db/ 1> /dev/null
+$udclient addVersionFiles -component stocktrader-all-in-one.yaml -version v1 -base $STOCKTRADER_LOCATION/Component/v1/ > /dev/null
+$udclient addVersionFiles -component stocktrader-all-in-one.yaml -version v1-onprem-db -base $STOCKTRADER_LOCATION/Component/v1-onprem-db/ > /dev/null
 
 echo creating application
 echo
@@ -88,7 +88,7 @@ data=json.load(sys.stdin); print data['id']"`
 echo adding component to application
 echo
 
-$udclient addComponentToApplication -component stocktrader-all-in-one.yaml -application StockTrader 1> /dev/null
+$udclient addComponentToApplication -component stocktrader-all-in-one.yaml -application StockTrader > /dev/null
 
 echo creating environments
 echo
@@ -111,20 +111,20 @@ echo "{
   \"templateName\": \"PROD\"
 }" > $STOCKTRADER_LOCATION/tmp/prodenv.json
 
-$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/localenv.json 1> /dev/null
-$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/qaenv.json 1> /dev/null
-$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/prodenv.json 1> /dev/null
+$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/localenv.json > /dev/null
+$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/qaenv.json > /dev/null
+$udclient createEnvironmentFromTemplate $STOCKTRADER_LOCATION/tmp/prodenv.json > /dev/null
 
 echo adding component templates to team
 echo
 
-$udclient addComponentTemplateToTeam -componentTemplate "Kubernetes Component Template" -team "Container Team" 1> /dev/null
-$udclient addComponentTemplateToTeam -componentTemplate "Docker Template" -team "Container Team" 1> /dev/null
+$udclient addComponentTemplateToTeam -componentTemplate "Kubernetes Component Template" -team "Container Team" > /dev/null
+$udclient addComponentTemplateToTeam -componentTemplate "Docker Template" -team "Container Team" > /dev/null
 
 echo adding resources to team
 echo
 
-$udclient addResourceToTeam -resource "/StockTrader" -team "Container Team" 1> /dev/null
+$udclient addResourceToTeam -resource "/StockTrader" -team "Container Team" > /dev/null
 
 echo removing tmp directory
 echo
